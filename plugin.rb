16a09821a1ecbafb8e45809519431abd676f6bf5
Angus McLeod <angus@mcleod.org.au>
2018-06-14 14:24:21 +1000

    init

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: civically-question-answer-extension
+# about: Adds additional functionality to the Question Answer Plugin
+# version: 0.1
+# authors: Angus McLeod
+# url: https://github.com/civicallyhq/x-civically-question-answer
